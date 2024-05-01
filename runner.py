64e9b965eabdff61278b7ca78f1d73c9dcacc76d
Princewill Chibuike <44113552+Emehprincewill@users.noreply.github.com>
2024-05-01 00:52:45 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Redirect to the desired web page
-    return redirect("https://6590-197-210-78-111.ngrok-free.app/")
+    return redirect("https://a42d-102-90-66-30.ngrok-free.app/")
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
